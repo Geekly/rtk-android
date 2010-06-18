25899d89e00026c8df8125878106915d1d997ee0
Geekly <khooks@gmail.com>
2010-06-17 22:55:01 -0400

    Initial Commit

---
+++ src/net/geeklythings/android/rtk/RtkActivity.java
@@ -0,0 +1,13 @@
+package net.geeklythings.android.rtk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RtkActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
